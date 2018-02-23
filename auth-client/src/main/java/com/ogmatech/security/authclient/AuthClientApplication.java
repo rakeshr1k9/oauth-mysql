9aad46ba8f1aac95c034db2f586fff0ed83745ab
rakeshr1k9 <rakeshr1k9@gmail.com>
2018-02-23 21:57:35 +0530

    Fisrt commit

---
+++ auth-client/src/main/java/com/ogmatech/security/authclient/AuthClientApplication.java
@@ -0,0 +1,12 @@
+package com.ogmatech.security.authclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthClientApplication.class, args);
+	}
+}
